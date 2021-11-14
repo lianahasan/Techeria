76fdbec8e955ea0fea7561dee77ee0687b16d755
lianahasan <74475156+lianahasan@users.noreply.github.com>
2021-11-14 06:49:39 -0500

    wsgi change

--- techeria_controller/techeria_controller/wsgi.py
+++ techeria_controller/techeria_controller/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techeria_controller.techeria_controller.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techeria_controller.settings')
 
 application = get_wsgi_application()
