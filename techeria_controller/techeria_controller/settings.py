237bafa933a6f068c6baaa4c2c4829baa8a0fe29
lianahasan <74475156+lianahasan@users.noreply.github.com>
2021-11-14 07:36:17 -0500

    Fixing deployment errors 2

--- techeria_controller/techeria_controller/settings.py
+++ techeria_controller/techeria_controller/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-import urlparse
+from urllib.parse import urlparse
 import django_heroku
 #import dj_database_url
 
