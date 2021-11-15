1973c68fc4382595cb5e129c8735f6737708dfae
lianahasan <74475156+lianahasan@users.noreply.github.com>
2021-11-15 00:01:38 -0500

    Trying to make MySQL work on deployed website

--- techeria_controller/techeria_controller/settings.py
+++ techeria_controller/techeria_controller/settings.py
@@ -158,6 +158,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
