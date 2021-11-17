458c5a60b7e0710fc531b1712a2d201f386fd8b8
Mohammad K Islam <62070498+kabirulislam95@users.noreply.github.com>
2021-11-17 16:26:06 -0500

    Deleting the extra file

--- techeria_controller/techeria_app/views.py
+++ techeria_controller/techeria_app/views.py
@@ -216,8 +216,6 @@ def loginpage(request):
 def signUpButton(request):
     return render(request, 'registration.html')
 
-def reset_password_email(request):
-    return render(request, 'reset_password_email.html')
 
 def ChangePassword(request):
     return render(request, 'ChangePassword.html')
