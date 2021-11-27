035764cd6710e52b3f22772656250fbd401db008
Liana Hasan <74475156+lianahasan@users.noreply.github.com>
2021-11-27 15:03:56 -0500

    Update settings.py

--- techeria_controller/techeria_controller/settings.py
+++ techeria_controller/techeria_controller/settings.py
@@ -205,10 +205,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-django_heroku.settings(locals())
-# del DATABASES['default']['OPTIONS']['sslmode']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
