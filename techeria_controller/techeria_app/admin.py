ca80efb5204ae2915265411707130b32434aef5e
Liana Hasan <74475156+lianahasan@users.noreply.github.com>
2021-11-29 21:15:20 -0500

    Revert "Mk secure registration. "

--- techeria_controller/techeria_app/admin.py
+++ techeria_controller/techeria_app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from techeria_app.models import Products
-from authentication.models import BuyerModel
+from techeria_app.models import Products,BuyerModel
 
 admin.site.register(Products)
 admin.site.register(BuyerModel)
