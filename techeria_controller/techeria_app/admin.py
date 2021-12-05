963a54061e11ecc57e505e44fc2ea8535f1be8b2
farahh001 <fsultan001@citymail.cuny.edu>
2021-12-05 18:45:35 -0500

    pages with no direction fixed and payment is added

--- techeria_controller/techeria_app/admin.py
+++ techeria_controller/techeria_app/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from techeria_app.models import Products,BuyerModel, SellerModel
+from techeria_app.models import Products,BuyerModel, SellerModel ,Payment, Order, OrderItem, PlaceOrder
 
 admin.site.register(Products)
 admin.site.register(BuyerModel)
 admin.site.register(SellerModel)
 
+admin.site.register(Payment)
+admin.site.register(Order)
+admin.site.register(PlaceOrder)
+admin.site.register(OrderItem)
